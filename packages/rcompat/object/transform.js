5f1bd89356ffc0d869210d17974e8494f118e4d7
terrablue <102580937+terrablue@users.noreply.github.com>
2024-02-18 16:03:18 +0200

    object.transform: use Object.{fromEntries,entries}

--- packages/rcompat/object/transform.js
+++ packages/rcompat/object/transform.js
@@ -1,10 +1,8 @@
 import { is } from "rcompat/invariant";
 import { identity } from "rcompat/function";
-import from from "./from.js";
-import to from "./to.js";
 
 export default (object = {}, transformer = identity) => {
   is(object).object();
   is(transformer).function();
-  return from(transformer(to(object)));
+  return Object.fromEntries(transformer(Object.entries(object)));
 };
