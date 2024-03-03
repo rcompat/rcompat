f2ab1ceee11909a0a02b85e26a020b0b615f7d01
terrablue <102580937+terrablue@users.noreply.github.com>
2024-03-03 16:10:08 +0200

    string: add HTML and HTML.encode

---
+++ packages/rcompat/string/HTML.js
@@ -0,0 +1,11 @@
+export default {
+  escape(input) {
+    // https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#output-encoding-for-html-contexts
+    return input
+      .replaceAll("&", "&amp;")
+      .replaceAll("<", "&lt;")
+      .replaceAll(">", "&gt;")
+      .replaceAll("\\\"", "&quot;")
+      .replaceAll("'", "&#x27;");
+  },
+};
--- packages/rcompat/string/exports.js
+++ packages/rcompat/string/exports.js
@@ -1,4 +1,5 @@
 export { default as Base64 } from "./Base64.js";
+export { default as HTML } from "./HTML.js";
 export { default as camelcased } from "./camelcased.js";
 export { default as globify } from "./globify.js";
 export { default as upperfirst } from "./upperfirst.js";
