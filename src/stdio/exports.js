3822fb9886fb68dd89a34c9d40814e0ec19e19ac
terrablue <102580937+terrablue@users.noreply.github.com>
2024-05-19 00:52:36 +0200

    convert /stdio

--- src/stdio/exports.js
+++
@@ -1,9 +0,0 @@
-export { stdin, stdout, stderr } from "node:process";
-import { exec } from "node:child_process";
-
-const execute = (command, options) => new Promise((resolve, reject) => {
-  exec(command, options, (error, stdout, stderr) =>
-    error === null ? resolve(stdout) : reject(stderr));
-});
-
-export { execute };
