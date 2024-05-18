cbb120d0d97f7f60c2636992fe091bc11a7a6d8e
terrablue <102580937+terrablue@users.noreply.github.com>
2024-05-19 00:47:12 +0200

    convert /package

--- src/package/exports.ts
+++ src/package/exports.ts
@@ -1,7 +1,7 @@
 import { File } from "rcompat/fs";
 import { manifest } from "rcompat/meta";
 
-const resolve = async url =>
+const resolve = async (url: string) =>
   (await File.discover(url, manifest)).join(manifest).json();
 
 export { resolve };
