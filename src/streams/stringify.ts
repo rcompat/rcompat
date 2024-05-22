48e70a6fefc73de0cf0a6e5c2c02ffe53944e176
terrablue <102580937+terrablue@users.noreply.github.com>
2024-05-22 23:41:04 +0200

    port /streams

--- src/streams/stringify.ts
+++ src/streams/stringify.ts
@@ -1,12 +1,13 @@
 import { defined } from "rcompat/function";
+import type { ReadableStreamDefaultReader } from "node:stream/web";
 import { ReadableStream } from "./exports.js";
 
 const decoder = new TextDecoder();
-const read = ({ chunks = [], reader }) =>
+const read = ({ chunks = [], reader } : { chunks?: string[], reader: ReadableStreamDefaultReader }): Promise<string[]> =>
   reader.read().then(({ done, value }) => done
     ? chunks
     : read({ chunks: [...chunks, decoder.decode(value)], reader }));
 
-export default async input => input instanceof ReadableStream
+export default async (input: ReadableStream | unknown) => input instanceof ReadableStream
   ? (await read({ reader: input.getReader() })).filter(defined).join("")
   : input;
