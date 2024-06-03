febf71f008e099ceb3da6b5640593b1591d336e9
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-04 01:43:18 +0200

    add /array (with array/empty)

---
+++ src/array/empty.spec.ts
@@ -0,0 +1,14 @@
+import empty from "./empty.js";
+
+export default (test => {
+  test.case("no params", assert => {
+    assert(empty(undefined as never)).throws();
+  });
+  test.case("empty array", assert => {
+    assert(empty([])).true();
+  });
+  test.case("non empty object", assert => {
+    assert(empty(["foo"])).equals(false);
+    assert(empty([["foo"]])).equals(false);
+  });
+}) satisfies DebrisTestSuite;
