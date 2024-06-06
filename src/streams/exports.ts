bae984a0d1f6974fd4141f8eb8ea0f23ce130092
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-06 02:08:29 +0200

    streams: Drop reexposing WHATWG objects

--- src/streams/exports.ts
+++ src/streams/exports.ts
@@ -1,10 +1,1 @@
-import * as node_streams from "node:stream/web";
-import { runtime } from "rcompat/meta";
-
-const is_bun = runtime === "bun";
-const RS = is_bun ? ReadableStream : node_streams.ReadableStream;
-const WS = is_bun ? WritableStream : node_streams.WritableStream;
-const TS = is_bun ? TransformStream : node_streams.TransformStream;
-
-export { RS as ReadableStream, WS as WritableStream, TS as TransformStream };
 export { default as stringify } from "./stringify.js";
