a0eac6c5b3e2dc46fa9019448f68f666a6c0a4f5
terrablue <102580937+terrablue@users.noreply.github.com>
2024-06-08 02:36:55 +0200

    add package/packager

--- src/package/exports.ts
+++ src/package/exports.ts
@@ -1,6 +1,8 @@
 import manifest from "./manifest.js";
+import packager from "./packager.js";
 import platform from "./platform.js";
 import resolve from "./resolve.js";
 import root from "./root.js";
 
-export { manifest, platform, resolve, root };
+export { manifest, packager, platform, resolve, root };
+
