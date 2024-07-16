b0f35b0bd87a144022b491a22a6ad0780721f3bc
terrablue <102580937+terrablue@users.noreply.github.com>
2024-07-16 02:03:22 +0200

    array.difference: add test

--- packages/array/src/difference.spec.ts
+++ packages/array/src/difference.spec.ts
@@ -8,6 +8,7 @@ export default (test => {
     assert(difference([0, 1], [])).equals([0, 1]);
     assert(difference([1, 0], [0, 1])).equals([1, 0]);
     assert(difference([0, 1, 2], [1])).equals([0, 2]);
+    assert(difference([1, 3, 5, 7, 9], [1, 4, 9])).equals([3, 5, 7]);
   });
 
 }) satisfies DebrisTestSuite;
