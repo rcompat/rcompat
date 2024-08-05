783e40975f59df8568221743d44feccd90ef282e
terrablue <102580937+terrablue@users.noreply.github.com>
2024-08-05 17:35:19 +0200

    stdio: add spawn

---
+++ packages/stdio/src/spawn/index.ts
@@ -0,0 +1,16 @@
+import { type SpawnOptions, spawn } from "node:child_process";
+import { Readable, Writable } from "node:stream";
+
+export default (command: string, options: SpawnOptions) => {
+  const { stdin, stdout, stderr } = spawn(command, {
+    ...options,
+    shell: true,
+    stdio: ["pipe", "pipe", "pipe"],
+  });
+
+  return {
+    stdin: Writable.toWeb(stdin),
+    stdout: Readable.toWeb(stdout),
+    stderr: Readable.toWeb(stderr),
+  };
+}
