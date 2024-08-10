ae24e8e75456918bdfd028200655d6c72c98eaf7
terrablue <102580937+terrablue@users.noreply.github.com>
2024-08-10 12:34:32 +0200

    add @rcompat/html

---
+++ packages/html/src/escape/index.ts
@@ -0,0 +1,9 @@
+export default (input: string): string => {
+  // https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html#output-encoding-for-html-contexts
+  return input
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll("\\\"", "&quot;")
+    .replaceAll("'", "&#x27;");
+};
