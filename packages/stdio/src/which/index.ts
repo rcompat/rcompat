888e0b71f2426de4e79dc21c850a64660e328afc
terrablue <102580937+terrablue@users.noreply.github.com>
2024-11-18 17:06:31 +0100

    stdio: add which

---
+++ packages/stdio/src/which/index.ts
@@ -0,0 +1,8 @@
+import execute from "@rcompat/stdio/execute";
+
+const is_win = process.platform === "win32";
+const which = is_win ? "where" : "which";
+const qualify = (path: string) => is_win ? `"${path}"` : path;
+
+export default async (command: string) =>
+  qualify(await execute(`${which} ${command}`, {})).replaceAll("\n", "");
