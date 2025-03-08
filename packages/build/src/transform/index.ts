d44f97520ae921e8f0db940095c95096548c8f30
terrablue <102580937+terrablue@users.noreply.github.com>
2025-03-08 03:15:33 +0100

    build/transform: simplify

--- packages/build/src/transform/index.ts
+++ packages/build/src/transform/index.ts
@@ -1,16 +1,1 @@
-import { transform } from "esbuild";
-
-type Options = {
-  tsconfig?: object
-}
-
-const make_options = (options: Options = {}) => {
-  const { tsconfig, ...rest } = options;
-
-  return tsconfig !== undefined
-    ? { tsconfigRaw: JSON.stringify(tsconfig), ...rest }
-    : rest;
-};
-
-export default (text: string, options: Options) =>
-  transform(text, make_options(options));
+export { transform as default } from "esbuild";
