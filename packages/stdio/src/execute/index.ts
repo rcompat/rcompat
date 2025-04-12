34fa44217bc1126fe3317f5b27a121e7045ffbcc
terrablue <102580937+terrablue@users.noreply.github.com>
2025-04-12 16:16:15 +0200

    stdio/execute: make options optional

--- packages/stdio/src/execute/index.ts
+++ packages/stdio/src/execute/index.ts
@@ -1,7 +1,7 @@
 import { type ExecOptions, exec } from "node:child_process";
 
-export default (command: string, options: ExecOptions): Promise<string> => 
+export default (command: string, options?: ExecOptions): Promise<string> =>
   new Promise((resolve, reject) => {
-    exec(command, options, (error, stdout, stderr) =>
+    exec(command, options ?? {}, (error, stdout, stderr) =>
       error === null ? resolve(stdout) : reject(stderr));
 });
