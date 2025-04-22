6dea6bbd4448fe296d2e285c57c5127f72991427
terrablue <102580937+terrablue@users.noreply.github.com>
2025-04-23 00:29:09 +0200

    test: 0.1.8

--- packages/test/src/private/Assert.ts
+++ packages/test/src/private/Assert.ts
@@ -24,7 +24,7 @@ export default class Assert<T> {
     this.#report(false, expected, actual);
   }
 
-  equals(expected: T) {
+  equals<Expected extends T>(expected: Expected) {
     this.#report(equals(this.#actual, expected), expected);
   }
 
