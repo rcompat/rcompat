2ea4828a0d51bc45e82efb318b9cbafdf4a1c1ea
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-18 18:23:48 +0200

    sqlite: expose Param type

--- packages/sqlite/src/private/api/Param.ts
+++ packages/sqlite/src/private/api/Param.ts
@@ -1,5 +1,6 @@
 import type PrimitiveParam from "#api/PrimitiveParam";
+import type Dict from "@rcompat/type/Dict";
 
-type Param = PrimitiveParam | Record<string, PrimitiveParam>;
+type Param = PrimitiveParam | Dict<PrimitiveParam>;
 
 export type { Param as default };
