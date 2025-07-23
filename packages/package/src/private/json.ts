f323ddb66c67522090495bcf62239dbfd13b12fe
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-23 20:52:05 +0200

    package: 0.13.1

--- packages/package/src/private/json.ts
+++ packages/package/src/private/json.ts
@@ -1,8 +1,3 @@
 import root from "#root";
-import maybe from "@rcompat/assert/maybe";
 
-export default async (from?: string) => {
-  maybe(from).string();
-
-  return (await root(from)).join("package.json");
-};
+export default (await root()).join("package.json");
