55e5847e904e91eb01a611a04f04c6b5427c1167
terrablue <102580937+terrablue@users.noreply.github.com>
2025-07-28 01:21:37 +0200

    package: support from in json

--- packages/package/src/private/json.ts
+++ packages/package/src/private/json.ts
@@ -1,3 +1,8 @@
 import root from "#root";
+import maybe from "@rcompat/assert/maybe";
 
-export default (await root()).join("package.json");
+export default async (from?: string) => {
+  maybe(from).string();
+
+  return (await root(from)).join("package.json");
+};
