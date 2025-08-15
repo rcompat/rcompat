135b2f390c406e3784e07b4375d5a4bbfd0a83f2
terrablue <102580937+terrablue@users.noreply.github.com>
2025-08-15 19:34:02 +0200

    is/integer: ignore bigints

--- packages/is/src/private/integer.ts
+++ packages/is/src/private/integer.ts
@@ -1,11 +1,5 @@
-export default function isInteger(x: unknown): x is number | bigint {
-  if (typeof x === "bigint") {
-    return true;
-  }
-
-  if (typeof x === "number") {
-    return Number.isInteger(x);
-  }
+export default function isInteger(x: unknown): x is number {
+  if (typeof x === "number") return Number.isInteger(x);
 
   return false;
 }
