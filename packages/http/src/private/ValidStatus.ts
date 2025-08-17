7917b4e74c84dfc3b50b14fe9edcff17dc2e77ad
terrablue <102580937+terrablue@users.noreply.github.com>
2025-08-17 20:44:22 +0200

    http: cleanup

--- packages/http/src/private/ValidStatus.ts
+++ packages/http/src/private/ValidStatus.ts
@@ -2,6 +2,6 @@ import type Status from "#Status";
 
 type StatusType = typeof Status;
 
-type Known = StatusType[keyof StatusType];
+type ValidStatus = StatusType[keyof StatusType];
 
-export type { Known as default };
+export type { ValidStatus as default };
