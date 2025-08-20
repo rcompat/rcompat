5edd2a19e105512b5e462c57c90ba22cc3e104f0
terrablue <102580937+terrablue@users.noreply.github.com>
2025-08-20 19:56:15 +0200

    cli: add mark

---
+++ packages/cli/src/private/mark.ts
@@ -0,0 +1,16 @@
+import dim from "#color/dim";
+import FileRef from "@rcompat/fs/FileRef";
+
+const stringify = (params: unknown[]) => params.map(param => {
+  if (param instanceof FileRef) {
+    return param.toString();
+  }
+  if (param instanceof Error) {
+    return param.message;
+  }
+  return param as string;
+});
+
+export default (format: string, ...params: unknown[]) =>
+  stringify(params).reduce((formatted, param, i) =>
+    formatted.replace(`{${i}}`, dim(param)), format);
