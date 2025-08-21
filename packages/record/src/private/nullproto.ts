0dace1b743833ebe2190f7f6088218c7fb9d89b3
terrablue <102580937+terrablue@users.noreply.github.com>
2025-08-21 18:31:43 +0200

    record: add nullproto

---
+++ packages/record/src/private/nullproto.ts
@@ -0,0 +1,48 @@
+interface Options {
+  frozen?: boolean;
+}
+
+function hasDepth(o: object): boolean {
+  const keys = [
+    ...Object.getOwnPropertyNames(o),
+    ...Object.getOwnPropertySymbols(o) as (string | symbol)[],
+  ];
+
+  for (const key of keys) {
+    const descriptor = Object.getOwnPropertyDescriptor(o, key);
+    // skip accessors
+    if (descriptor === undefined || !("value" in descriptor)) continue;
+
+    const value = descriptor.value;
+    if (value !== null && typeof value === "object") {
+      // Any object counts as depth > 1
+      return true;
+    }
+  }
+  return false;
+}
+
+export default function nullproto(): Record<PropertyKey, never>;
+export default function nullproto<T extends object>(
+  init: T,
+  options: { frozen: true }
+): Readonly<T>;
+export default function nullproto<T extends object>(init: T): T;
+export default function nullproto<T extends object>(
+  init?: T,
+  options?: Options,
+) {
+  const hasInit = init !== undefined;
+
+  if (options?.frozen && hasInit && hasDepth(init)) {
+    throw new TypeError(
+      "nullproto: cannot freeze a non-flat object",
+    );
+  }
+
+  const o: T = Object.create(null);
+
+  hasInit && Object.defineProperties(o, Object.getOwnPropertyDescriptors(init));
+
+  return options?.frozen ? Object.freeze(o) : o;
+}
