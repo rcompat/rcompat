0446ab0cb9bc3a5381b0f35d6d1f15d234f2432f
terrablue <102580937+terrablue@users.noreply.github.com>
2025-11-10 23:42:49 +0100

    crypto/hash/node: support ArrayBuffer and ArrayBufferView

--- packages/crypto/src/private/hash/node.ts
+++ packages/crypto/src/private/hash/node.ts
@@ -1,13 +1,23 @@
 import crypto from "node:crypto";
 
 const encoder = new TextEncoder();
-export default async (string: string, algorithm = "sha-256") => {
+
+export default async function hash(
+  data: string | ArrayBuffer | ArrayBufferView,
+  algorithm = "SHA-256",
+): Promise<string> {
+  const toDigest = typeof data === "string"
+    ? encoder.encode(data)
+    : data;
+
+  const digest = await crypto.subtle.digest(algorithm, toDigest);
+
   const base = 16;
   const target_pad_length = 2;
   const target_slice = 20;
-  const bytes = await crypto.subtle.digest(algorithm, encoder.encode(string));
-  return Array.from(new Uint8Array(bytes))
+
+  return Array.from(new Uint8Array(digest))
     .map(byte => byte.toString(base).padStart(target_pad_length, "0"))
     .join("")
     .slice(0, target_slice) + "n";
-};
+}
