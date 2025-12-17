9121cb780d2c9b43f4cbf9e53b2f68185978bfce
terrablue <102580937+terrablue@users.noreply.github.com>
2025-12-17 08:44:18 +0100

    dict: improve perf

--- packages/dict/src/private/get.ts
+++ packages/dict/src/private/get.ts
@@ -1,17 +1,18 @@
 import type Dict from "@rcompat/type/Dict";
 
-type GetResultType<T, P, D extends number[] = []> =
-  D["length"] extends 6
+export interface GetResultType<T, P, D extends number[] = []> {
+  result: D["length"] extends 6
   ? unknown
   : P extends `${infer P1}.${infer Rest}`
   ? P1 extends keyof T
-  ? GetResultType<Exclude<T[P1], undefined>, Rest, [...D, 0]> | (T[P1] & undefined)
+  ? GetResultType<Exclude<T[P1], undefined>, Rest, [...D, 0]>["result"] | (T[P1] & undefined)
   : undefined
   : P extends keyof T
   ? T[P]
   : undefined;
+}
 
 export default <T extends Dict, P extends string>(dict: T, path: P):
-  GetResultType<T, P> =>
+  GetResultType<T, P>["result"] =>
   path.split(".").reduce((subobject, key) =>
     (subobject as never)[key], dict) as never;
