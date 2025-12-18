3e516322c89cd9f480e8af350572421cab299f7d
terrablue <102580937+terrablue@users.noreply.github.com>
2025-12-18 11:56:24 +0100

    add bump script

---
+++ scripts/bump.js
@@ -0,0 +1,25 @@
+import root from "@rcompat/fs/project/root";
+
+const packages = await (await root()).join("packages").list();
+
+// check all first
+for (const pkg of packages) {
+  const json = await pkg.join("package.json").json();
+  if (json.version.includes("-")) {
+    console.error(`package ${json.name} already set to dev: ${json.version}`);
+    break;
+  }
+  const [major, minor, patch] = json.version.split(".").map(Number);
+  if (major > 0) {
+    console.error(`still in <1 phase, adapt script if this is no longer the case`);
+    break;
+  }
+}
+
+for (const pkg of packages) {
+  const package_json = pkg.join("package.json");
+  const json = await package_json.json();
+  const [major, minor, patch] = json.version.split(".").map(Number);
+  json.version = `${major}.${minor + 1}.0`;
+  await package_json.writeJSON(json);
+}
