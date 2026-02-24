9f79528bbe22e2996091248da246498640504f28
terrablue <102580937+terrablue@users.noreply.github.com>
2026-02-24 01:59:19 +0100

    dict.toQueryString: add invariant check

--- packages/dict/src/private/toQueryString.ts
+++ packages/dict/src/private/toQueryString.ts
@@ -1,6 +1,9 @@
+import is from "@rcompat/is";
 import type { Dict } from "@rcompat/type";
 
 export default function toQueryString(dict: Dict<string>) {
+  is.dict(dict);
+
   return Object.entries(dict)
     .map(([key, value]) => `${key}=${value}`)
     .join("&");
