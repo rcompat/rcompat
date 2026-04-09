2f4dc90b97a11b025f1a92d51bb3f84c9af52399
terrablue <102580937+terrablue@users.noreply.github.com>
2026-04-09 19:19:43 +0200

    runtime: allow strings in packageJSON and projectRoot

--- packages/runtime/src/public/common.ts
+++ packages/runtime/src/public/common.ts
@@ -1,13 +1,13 @@
-import type { FileRef } from "@rcompat/fs";
+import type { Path } from "@rcompat/fs";
 import fs from "@rcompat/fs";
 import type { PackageJSON } from "@rcompat/type";
 
-async function root(from: FileRef = fs.cwd()) {
-  return from.discover("package.json");
+async function root(from: Path = fs.cwd()) {
+  return fs.ref(from).discover("package.json");
 }
 
-async function packageJSON(from: FileRef = fs.cwd()): Promise<PackageJSON> {
-  return (await root(from)).join("package.json").json<PackageJSON>();
+async function packageJSON(from: Path = fs.cwd()): Promise<PackageJSON> {
+  return (await root(fs.ref(from))).join("package.json").json<PackageJSON>();
 }
 
 export default {
